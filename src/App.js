b49aecbcf28d71ae0546d623e2f2dbd065b8718a
Hannes <h.geissler@schille.com>
2019-12-17 10:34:56 +0100

    state, render app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import 'materialize-css'
+import axios from 'axios'
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null,
+  }
+  
+  render () {
+    const { data } = this.state
+    return (
     <div>
       LETS GO
     </div>
-  );
+    )
+  };
 }
 
 export default App;
